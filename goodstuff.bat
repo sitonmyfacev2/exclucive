658978ba82fc3d6022806ffeb173591fd5d911f5
sitonmyfavev2 <143049829+sitonmyfacev2@users.noreply.github.com>
2023-08-28 00:23:15 -0400

    Add files via upload

---
+++ goodstuff.bat
@@ -0,0 +1,65 @@
+@echo off
+chcp 65001 >nul
+mode 170, 35
+:login
+color 5
+cls
+title exlucive by / .sitonmyface.
+echo ░░       ░░░░░░   ░░░░░░  ░░ ░░░    ░░ 
+echo ▒▒      ▒▒    ▒▒ ▒▒       ▒▒ ▒▒▒▒   ▒▒ 
+echo ▒▒      ▒▒    ▒▒ ▒▒   ▒▒▒ ▒▒ ▒▒ ▒▒  ▒▒ 
+echo ▓▓      ▓▓    ▓▓ ▓▓    ▓▓ ▓▓ ▓▓  ▓▓ ▓▓ 
+echo ███████  ██████   ██████  ██ ██   ████ 
+echo      made by .sitonmyface.
+echo ----------------------------------------
+echo if u want it dm .sitonmyface. on discord
+echo ----------------------------------------
+echo.
+
+set /p user=username:
+echo >nul
+set /p pass=password:
+if %user% == root  if %pass% == beer  goto main
+echo Error 04, sorry it seems like u typed the wrong user or pass!
+timeout 2 >nul
+goto login
+:main
+echo welcome %USERNAME%
+cls 
+title devils rejects / by .sitonmyface.
+echo ███████╗██╗  ██╗ ██████╗██╗     ██╗   ██╗ ██████╗██╗██╗   ██╗███████╗
+echo ██╔════╝╚██╗██╔╝██╔════╝██║     ██║   ██║██╔════╝██║██║   ██║██╔════╝
+echo █████╗   ╚███╔╝ ██║     ██║     ██║   ██║██║     ██║██║   ██║█████╗  
+echo ██╔══╝   ██╔██╗ ██║     ██║     ██║   ██║██║     ██║╚██╗ ██╔╝██╔══╝  
+echo ███████╗██╔╝ ██╗╚██████╗███████╗╚██████╔╝╚██████╗██║ ╚████╔╝ ███████╗
+echo ╚══════╝╚═╝  ╚═╝ ╚═════╝╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═══╝  ╚══════╝
+echo .                
+echo ==========================================================================================================================================================================
+echo -   onlyfans/deepfake porn                                     youtuberleakes/deepfakes porn
+echo -
+echo -  1 ♥belle delphine Leaks                                     6 ssniperwolf
+echo -
+echo -  2 ♥jenny ortega deep fake leaks 
+echo -                                                             
+echo -  3 ♥sexy redbone only fan leaks             50 exit
+echo -              
+echo -  4 cardi b onlyfans leaks
+echo -
+echo -  5 bhad baby only fans leaks  
+echo ==========================================================================================================================================================================
+echo. 
+:tools 
+set /p main= enter option 
+if %main% == 1 start https://www.erome.com/search?q=belle_delphine BELE DELPHINE LEAKS
+
+if %main% == 2 start https://www.erome.com/search?q=wednesday 
+
+if %main% == 3 start Redbones https://spankbang.com/s/redbone+onlyfans/
+
+if %main% == 50 start MAIN.bat
+
+if %main% == 4 start https://www.erome.com/a/SdAjGeOp
+
+if %main% == 5 start https://www.erome.com/a/03W4mXIZ
+
+if %main% == 6 start https://mrdeepfakes.com/tags/youtuber
